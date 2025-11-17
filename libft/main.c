130db870cdec3642dfe71f0255b7663bdf510c00
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-11-17 21:17:38 +0100

    add headers and make commands to build libraries

--- libft/main.c
+++ libft/main.c
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 21:17:24 by jhoban            #+#    #+#             */
+/*   Updated: 2025/11/17 21:17:25 by jhoban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdio.h>
 
 int main(void)
 {
     char str[50] = "Hello, World!";
-    ft_bzero(str, 5);
+    printf("Original string: '%s'\n", str);
+    bzero(str, sizeof(char) * 5);
     // Should print ", World!"
     printf("Resulting string: '%s'\n", str); 
     return 0;
