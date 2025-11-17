417ca263b274940571ea9dd175e250d45c43f772
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-11-17 21:40:27 +0100

    add test file

--- libft/test.c
+++ libft/test.c
@@ -1,24 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 21:17:24 by jhoban            #+#    #+#             */
-/*   Updated: 2025/11/17 21:17:25 by jhoban           ###   ########.fr       */
+/*   Updated: 2025/11/17 21:38:32 by jhoban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+
+int test_ft_bzero(void)
+{
+    char str1[] = "Hello, World!";
+    char str2[] = "Hello, World!";
+    ft_bzero(str1, sizeof(char) * 5);
+    bzero(str2, sizeof(char) * 5);
+    if (memcmp(str1, str2, sizeof(str1)) == 0)
+        return 1; // Test passed
+    return 0; // Test failed
+}
+
 int main(void)
 {
-    char str[50] = "Hello, World!";
-    printf("Original string: '%s'\n", str);
-    bzero(str, sizeof(char) * 5);
-    // Should print ", World!"
-    printf("Resulting string: '%s'\n", str); 
+    if (test_ft_bzero())
+        printf("ft_bzero test passed!\n");
+    else
+        printf("ft_bzero test failed!\n");
     return 0;
 }
