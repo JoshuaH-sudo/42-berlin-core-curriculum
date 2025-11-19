35887f02fcb27cf4ffc0d3ebe14965dd722d160c
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-11-19 17:14:38 +0100

    fix: update timestamp in header comment and add new function prototypes for ft_putchar_fd, ft_putstr_fd, ft_putendl_fd, and ft_putnbr_fd

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 21:17:05 by jhoban            #+#    #+#             */
-/*   Updated: 2025/11/19 16:47:09 by jhoban           ###   ########.fr       */
+/*   Updated: 2025/11/19 17:14:21 by jhoban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,9 @@ char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
   
 #endif // LIBFT_H
