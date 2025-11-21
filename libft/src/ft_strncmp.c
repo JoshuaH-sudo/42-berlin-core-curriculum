893ff9d04e4717d565d374e4047aec2901161f44
Joshua-Hoban-visi-one <j.hoban@visi-one.com>
2025-11-21 16:48:47 +0100

    fix: update author email in ft_strncmp.c and improve pointer usage for string comparison

--- libft/src/ft_strncmp.c
+++ libft/src/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhoban <jhoban@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 15:44:11 by jhoban            #+#    #+#             */
-/*   Updated: 2025/11/20 12:05:21 by jhoban           ###   ########.fr       */
+/*   Updated: 2025/11/21 16:40:17 by jhoban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	index;
+	unsigned char *uc_s1;
+	unsigned char *uc_s2;
 
+	uc_s1 = (unsigned char *)s1;
+	uc_s2 = (unsigned char *)s2;
 	index = 0;
-	while ((s1[index] || s2[index]) && index < n)
+	while ((uc_s1[index] || uc_s2[index]) && index < n)
 	{
-		if ((unsigned char)s1[index] != (unsigned char)s2[index])
-			return (s1[index] - s2[index]);
+		if (uc_s1[index] != uc_s2[index])
+			return (uc_s1[index] - uc_s2[index]);
 		index++;
 	}
 	return (0);
