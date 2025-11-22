cf18eee317783b877c826656a93a54447666d2f4
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-11-22 17:42:16 +0100

    fix: refactor get_next_line for improved buffer handling and memory management

--- getnextline/src/get_next_line.c
+++ getnextline/src/get_next_line.c
@@ -6,146 +6,156 @@
 /*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 14:28:44 by jhoban            #+#    #+#             */
-/*   Updated: 2025/11/22 15:27:04 by jhoban           ###   ########.fr       */
+/*   Updated: 2025/11/22 17:29:18 by jhoban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-  size_t  len;
+	size_t	len;
 
-  len = 0;
-  while (s[len] != '\0')
-    len++;
-  return (len);
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
 
-int   find_newline(char *str)
+int	find_newline(char *str)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while ((unsigned char)str[i] != '\0')
-  {
-    if ((unsigned char)str[i] == '\n')
-      return (i);
-    i++;
-  }
-  return (-1);
+	i = 0;
+	while ((unsigned char)str[i] != '\0')
+	{
+		if ((unsigned char)str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
-char  *append_str(char *dest, char *src, size_t dest_len, size_t src_len)
+char	*append_str(char *dest, char *src, size_t dest_len, size_t src_len)
 {
-  char *new_str;
-  size_t i;
+	char	*new_str;
+	size_t	i;
+	size_t	j;
 
-  new_str = (char *)malloc(dest_len + src_len + 1);
-  if (!new_str)
-    return (NULL);
-  i = 0;
-  while (i < dest_len)
-  {
-    new_str[i] = dest[i];
-    i++;
-  }
-  size_t j = 0;
-  while (j < src_len)
-  {
-    new_str[i + j] = src[j];
-    j++;
-  }
-  new_str[dest_len + src_len] = '\0';
-  free(dest);
-  return (new_str);
+	new_str = (char *)malloc((dest_len + src_len + 1) * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (i < dest_len)
+	{
+		new_str[i] = dest[i];
+		i++;
+	}
+	j = 0;
+	while (j < src_len)
+	{
+		new_str[i + j] = src[j];
+		j++;
+	}
+	new_str[dest_len + src_len] = '\0';
+	return (new_str);
 }
 
-// char  *get_next_line(int fd)
-// {
-//   static char buffer[BUFFER_SIZE];
-//   static size_t leftover;
-//   ssize_t nread;
-//   char *line;
-//   int newline_index;
-
-//   leftover = 0;
-//   newline_index = -1;
-//   nread = 0;
-//   line = (char *)malloc(BUFFER_SIZE);
-//   if (fd < 0)
-//     return (NULL);
-//   if (leftover == 0)
-//   {
-//     nread = read(fd, buffer, BUFFER_SIZE); 
-//     if (nread < 0)
-//       return (NULL);
-//     buffer[nread] = '\0';
-//     leftover = nread;
-//   }
-//   while((nread = read(fd,buffer, 1)) > 0)
-//   {
-//     newline_index = find_newline(buffer);
-//     if (newline_index != -1)
-//     {
-//       line = append_str(line, buffer, ft_strlen(line), newline_index);
-//       break;
-//     }
-//     line = append_str(line, buffer, ft_strlen(line), ft_strlen(buffer));
-//   }
-//   leftover = 0;
-//   return (line);
-// }
-
-char *get_next_line(int fd)
+char	*ft_memmove(void *dest, const void *src, size_t n)
 {
-  static char *buffer;
-  ssize_t nread;
-  size_t buffer_len;
-  int newline_index;
-  char *line = NULL;
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
 
-  if (fd < 0)
-    return (NULL);
-  if (!buffer)
-  {
-    buffer = (char *)malloc(BUFFER_SIZE);
-    if (!buffer)
-      return (NULL);
+	d = dest;
+	s = src;
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			d[i - 1] = s[i - 1];
+			i--;
+		}
+	}
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char *buffer;
+	char *line;
+	char *line_new;
+	ssize_t nread;
+	// size_t buffer_len;
+	size_t leftover;
+	int newline_index;
+
+	newline_index = -1;
+	line = NULL;
+	if (fd < 0)
+		return (NULL);
+	if (!buffer)
+	{
+		buffer = (char *)malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
+		buffer[0] = '\0';
+	}
+	if (!line)
+	{
+		line = (char *)malloc(BUFFER_SIZE + 1);
+		if (!line)
+			return (NULL);
+		line[0] = '\0';
+	}
+  
+	if (find_newline(buffer) != -1)
+	{
+		line_new = append_str(line, buffer, ft_strlen(line), ft_strlen(buffer));
+		free(line);
+    return (line_new);
+		leftover = ft_strlen(buffer) - (newline_index + 1);
+		ft_memmove(buffer, buffer + newline_index + 1, leftover);
+	} else {
+    line_new = append_str(line, buffer, ft_strlen(line), ft_strlen(buffer));
+    free(line);
+    line = line_new;
     buffer[0] = '\0';
   }
-  if (!line)
-  {
-    line = (char *)malloc(1);
-    if (!line)
-      return (NULL);
-    line[0] = '\0';
-  }
-  buffer_len = ft_strlen(buffer);
-  if (buffer_len == 0)
-  {
-    nread = read(fd, buffer, BUFFER_SIZE);
-    if (nread < 0)
-    {
-      free(buffer);
-      return (NULL);
-    }
-    buffer[nread] = '\0';
-    buffer_len = ft_strlen(buffer);
-  }
-  if (buffer_len > 0)
-  {
-    printf("checking existing buffer: \n----\n%s\n----\n", buffer);
-    newline_index = find_newline(buffer);
-    if (newline_index != -1)
-    {
-      line = append_str(line, buffer, ft_strlen(line), newline_index);
-      buffer += (newline_index + 1);
-      return (line);
-    }
-  }
-  return (line);
+
+	while ((nread = read(fd, buffer, BUFFER_SIZE)) > 0 || find_newline(buffer) != -1)
+	{
+		buffer[nread] = '\0';
+		newline_index = find_newline(buffer);
+		if (newline_index != -1)
+		{
+			line_new = append_str(line, buffer, ft_strlen(line), newline_index
+					+ 1);
+			free(line);
+			line = line_new;
+			// shift leftover
+			leftover = ft_strlen(buffer) - (newline_index + 1);
+			ft_memmove(buffer, buffer + newline_index + 1, leftover);
+			buffer[leftover] = '\0';
+			return (line);
+		}
+		else
+		{
+			line_new = append_str(line, buffer, ft_strlen(line),
+					ft_strlen(buffer));
+			free(line);
+			line = line_new;
+		}
+	}
+
+	return (line);
 }
