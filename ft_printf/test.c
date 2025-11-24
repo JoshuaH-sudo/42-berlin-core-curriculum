23a98267537d3f1d663acd213636e54c024c0c24
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-11-24 18:21:29 +0100

    fix: update Makefile headers with correct timestamps and clean up test commands

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -32,30 +32,30 @@ int main()
     printf("ft_printf returned: %d\n", res1);
     printf("printf returned: %d\n", res2);
     
-    // printf("printf:\nc: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %%: %%\n", 'A', "test", ptr, dec_num, dec_num, unassigned_int, hex_num, hex_num);
-    // ft_printf("ft_printf:\nc: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %%: %%\n", 'A', "test", ptr, dec_num, dec_num, unassigned_int, hex_num, hex_num);
-    // printf("\n%%c / %%s ---\n");
-    // TEST("hello c: %c, s: %s AAAA\n", '!', "joshuahoban");
-    // printf("\n%%p ---\n");
-    // TEST("hello p: %p, p(null): %p\n", &dec_num, ptr);
-    // printf("\n%%d ---\n");
-    // TEST("fd(dec): %d, d(oct_num): %d, d(hex_num): %d\n", dec_num, oct_num, hex_num);
-    // printf("\n%%i ---\n");
-    // TEST("i(dec): %i, i(oct_num): %i, i(hex_num): %i\n", dec_num, oct_num, hex_num);
-    // printf("\n%%d ---\n");
-    // TEST("d(dec): %d, d(oct_num): %d, d(hex_num): %d\n", -dec_num, -oct_num, -hex_num);
-    // printf("\n%%i neg ---\n");
-    // TEST("i(dec): %i, i(oct_num): %i, i(hex_num): %i\n", -dec_num, -oct_num, -hex_num);
-    // printf("\n%%u ---\n");
-    // TEST("u(unsigned): %u\n", unassigned_int);
-    // printf("\n%%x %%X ---\n");
-    // TEST("x(lowercase hex): %x, X(uppercase hex): %X\n", hex_num, hex_num);
-    // printf("\n%% ---\n");
-    // TEST("percent sign: %%\n");
+    printf("printf:\nc: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %%: %%\n", 'A', "test", ptr, dec_num, dec_num, unassigned_int, hex_num, hex_num);
+    ft_printf("ft_printf:\nc: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %%: %%\n", 'A', "test", ptr, dec_num, dec_num, unassigned_int, hex_num, hex_num);
+    printf("\n%%c / %%s ---\n");
+    TEST("hello c: %c, s: %s AAAA\n", '!', "joshuahoban");
+    printf("\n%%p ---\n");
+    TEST("hello p: %p, p(null): %p\n", &dec_num, ptr);
+    printf("\n%%d ---\n");
+    TEST("fd(dec): %d, d(oct_num): %d, d(hex_num): %d\n", dec_num, oct_num, hex_num);
+    printf("\n%%i ---\n");
+    TEST("i(dec): %i, i(oct_num): %i, i(hex_num): %i\n", dec_num, oct_num, hex_num);
+    printf("\n%%d ---\n");
+    TEST("d(dec): %d, d(oct_num): %d, d(hex_num): %d\n", -dec_num, -oct_num, -hex_num);
+    printf("\n%%i neg ---\n");
+    TEST("i(dec): %i, i(oct_num): %i, i(hex_num): %i\n", -dec_num, -oct_num, -hex_num);
+    printf("\n%%u ---\n");
+    TEST("u(unsigned): %u\n", unassigned_int);
+    printf("\n%%x %%X ---\n");
+    TEST("x(lowercase hex): %x, X(uppercase hex): %X\n", hex_num, hex_num);
+    printf("\n%% ---\n");
+    TEST("percent sign: %%\n");
     
-    // if (result)
-    //     printf("\nAll tests passed!\n");
-    // else
-    //     printf("\nSome tests failed.\n");
+    if (result)
+        printf("\nAll tests passed!\n");
+    else
+        printf("\nSome tests failed.\n");
     return 0;
 }
