de1b59fb16ec44f62a2de53459d0559436f87a23
jhoban <jhoban@student.42berlin.de>
2025-11-27 18:36:07 +0100

    fix: update file header comment with the latest modification date

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jhoban <jhoban@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 14:27:38 by jhoban            #+#    #+#             */
-/*   Updated: 2025/11/27 17:24:39 by jhoban           ###   ########.fr       */
+/*   Updated: 2025/11/27 18:36:01 by jhoban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,5 @@ size_t	ft_strlen(const char *s);
 char	*ft_joinstr(char *dest, char *src);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*extract_line(char *line_buffer);
-char	*read_buffer(int fd, char *buffer, char *left_over);
 char	*get_next_line(int fd);
 #endif // GET_NEXT_LINE_H
