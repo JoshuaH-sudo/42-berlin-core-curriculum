c9a20f3547b7dee35b10ad29893768464e108737
jhoban <jhoban@student.42berlin.de>
2025-12-01 10:40:20 +0100

    fix: correct content in single_line_no_nl.txt and update test case execution order in test.c

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -31,11 +31,14 @@ void	run_test(char *filename)
 
 int	main(void)
 {
+	printf("----- get_next_line Test Suite -----\n\n");
+	printf("BUFFER_SIZE: %d\n\n", BUFFER_SIZE);
 	// TEST_FILE("test.txt");
 	// TEST_FILE("multiple_nl.txt");
-	// TEST_FILE("single_line_no_nl.txt");
+	TEST_FILE("single_line_no_nl.txt");
 	// TEST_FILE("empty.txt");
-	TEST_FILE("1char.txt");
+	// TEST_FILE("1char.txt");
 	// TEST_FILE("long_line.txt");
+	printf("----- End of Test Suite -----\n");
 	return (0);
 }
