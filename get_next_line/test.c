4a9263823081c7bf5866b38d7b8cf46699b08f4a
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-12-01 09:10:18 +0100

    fix: refactor test function to accept filename parameter and improve error handling

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -4,25 +4,35 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(void)
+void	run_test(char *filename)
 {
-	int     fd;
-	char    *line;
+	int		fd;
+	char	*line;
+	int		counter;
 
-	//fd = open("test.txt", O_RDONLY);
-	//fd = open("multiple_nl.txt", O_RDONLY);
-	fd = open("single_line_no_nl.txt", O_RDONLY);
+	printf("=== Testing %s ===\n", filename);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		perror("Error opening file");
-		return (1);
+		return ;
 	}
-	int counter = 1;
+	counter = 1;
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		printf("line %d: %s", counter++, line);
 		free(line);
 	}
 	close(fd);
+	printf("\n");
+}
+
+#define TEST_FILE(file) run_test(file)
+
+int	main(void)
+{
+	TEST_FILE("test.txt");
+	TEST_FILE("multiple_nl.txt");
+	TEST_FILE("single_line_no_nl.txt");
 	return (0);
 }
